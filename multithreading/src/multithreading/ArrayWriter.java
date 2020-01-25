d07fe081e162a9883151cd256e0f42fff4870ca7
Dayoon Lee <danadayoonlee@gmail.com>
2020-01-24 21:27:25 -0500

    initial commit

---
+++ multithreading/src/multithreading/ArrayWriter.java
@@ -0,0 +1,27 @@
+package dayoon_lee_exercise4;
+
+// Fig. 23.6: ArrayWriter.java
+// Adds integers to an array shared with other Runnables
+import java.lang.Runnable;
+import java.security.SecureRandom;
+
+public class ArrayWriter implements Runnable {
+   private final SimpleArray sharedSimpleArray;
+   private static final SecureRandom generator = new SecureRandom();
+   private final int startValue;
+
+   public ArrayWriter(int value, SimpleArray array) {
+      startValue = value;
+      sharedSimpleArray = array;
+   }
+
+   
+   @Override
+   public void run() {
+      for (int i = startValue; i < startValue + 3; i++) {
+    	  sharedSimpleArray.add(startValue+generator.nextInt(10));
+      } 
+   }
+} 
+
+
